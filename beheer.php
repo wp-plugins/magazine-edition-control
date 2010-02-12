7a600acea2f8c4c99f0b16857d6b014a9d5582c6
erwinwolff <erwinwolff@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-12 21:20:33 +0000

    sync
    git-svn-id: https://plugins.svn.wordpress.org/magazine-edition-control/trunk@204702 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- beheer.php
+++ beheer.php
@@ -30,6 +30,10 @@ add_action('activated_plugin', 'magazinedition_install');
 
 add_action('admin_menu', 'magazinedition_admin_actions');
 
+add_action('admin_menu', 'magazinedition_custom');
+
+add_action('save_post', 'magazinedition_save_postdata');
+
 
 function magazinedition_install()
 {
@@ -51,6 +55,11 @@ function magazinedition_install()
     $wpdb->query("ALTER TABLE " . $wpdb->prefix  . "magazinedition_uitgaven ADD PRIMARY KEY ( cat_date ) ");
 }
 
+function magazinedition_custom () {
+if ( current_user_can('manage_options') ) {
+add_meta_box("magazinedition_meta", __('Magazine Edtion Control', 'magazinedition'), "magazinedition_meta", "post", 'side', 'core');
+}
+}
 
 function magazinedition_menu()
 {
@@ -155,4 +164,68 @@ $return = $string;
 $return = str_replace("\\\"", "\"", $return);
 $return = str_replace("\\'", "'", $return);
 return $return;
+}
+
+function magazinedition_meta ( ) {
+ global $wpdb;
+global $post;
+
+$structure = "SELECT * FROM " . $wpdb->prefix . "magazinedition_uitgaven ORDER BY cat_date";
+$results = $wpdb->get_results($structure);
+
+$count = 0;
+foreach ($results as $result) {
+$count++;
+}
+if ($count == 0) {
+
+echo "<a href=\"" . get_bloginfo('siteurl')  . "/wp-admin/admin.php?page=magazinedition\">Please create and name an edition first.</a><br />";
+return 0;
+}
+
+$current_guid = get_post_meta( $_GET['post'], 'magazineditionuitgave', true);
+
+echo "<label for=\"magazinedition_save_guid\">Select an edition:</label><br />";
+echo "<select name=\"magazinedition_save_guid\" style=\"width: 90%\">\n";
+echo "<option value=\"\">This post isn't part of an edition.</option><br />\n";
+
+foreach ($results as $result) {
+$title = "";
+
+if ($result->cat_name == "") {
+$title = "Edition of " . $result->cat_date;
+} else {
+$title = $result->cat_name;
+}
+
+if ($result->cat_guid == $current_guid) {
+echo "<option value=\"" . $result->cat_guid  . "\" selected=\"true\">" . $title . "</option><br />\n";
+} else {
+echo "<option value=\"" . $result->cat_guid  . "\">" . $title . "</option><br />\n";
+
+}
+}
+echo "</select><br /><br /><br />";
+echo "<a href=\"" . get_bloginfo('siteurl')  . "/wp-admin/admin.php?page=magazinedition\">Edit editions.</a><br />";
+
+}
+
+
+function magazinedition_save_postdata ( $post_id ) {
+
+if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+    return $post_id;
+
+  if ( 'page' == $_POST['post_type'] ) {
+    if ( !current_user_can( 'edit_page', $post_id ) )
+      return $post_id;
+  } else {
+    if ( !current_user_can( 'edit_post', $post_id ) )
+      return $post_id;
+  }
+
+add_post_meta($post_id, 'magazineditionuitgave', $_POST['magazinedition_save_guid'], true) or update_post_meta( $post_id, 'magazineditionuitgave', $_POST['magazinedition_save_guid'] );
+
+return $post_id;
+
 }
